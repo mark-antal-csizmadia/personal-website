596178dfd22375921016e7dfe48ba8bddcd3e664
Mark Csizmadia <mark.antal.csizmadia@gmail.com>
2020-07-17 15:33:43 +0200

    Trying to deploy

--- django_personal_website/settings.py
+++ django_personal_website/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,8 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media root (.png, .jpg, etc.)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
