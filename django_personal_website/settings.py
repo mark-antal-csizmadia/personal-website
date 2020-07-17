104c548339ab980f8bbf9aebaa8a7faefa740e66
Mark Csizmadia <mark.antal.csizmadia@gmail.com>
2020-07-17 14:31:31 +0200

    Fixed static path

--- django_personal_website/settings.py
+++ django_personal_website/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Media root (.png, .jpg, etc.)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
